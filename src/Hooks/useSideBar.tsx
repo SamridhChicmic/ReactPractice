860ed05f05d16e1b20d637eb3900a8068c8e2888
SamridhChicmic <samridh.gupta@chicmic.co.in>
2024-08-01 18:26:17 +0530

    Custom Hook Side Bar

---
+++ src/Hooks/useSideBar.tsx
@@ -0,0 +1,29 @@
+import {  useParams,} from 'react-router-dom';
+import { PARAM_NAME } from '../Views/SideBar/helper/constant';
+import Agency from '../Views/SideBar/componets/Agency';
+import Credit from '../Views/SideBar/componets/Credit';
+import Notification from '../Views/SideBar/componets/Notification';
+import Test from '../Components/TestingComponent/Test';
+function useSideBar() {
+ const {tab}=useParams()
+ const renderPage=()=>{
+    switch (tab) {
+        case PARAM_NAME.AGENCY:
+        return <Agency/>
+        break;
+        case PARAM_NAME.NOTIFCATION:
+        return <Notification/>
+        break;
+        case PARAM_NAME.Credit:
+        return <Credit/>
+        default:
+            return <Test/>
+    }
+}
+  return {
+    tab,
+     renderPage
+  }
+}
+
+export default useSideBar
--- src/Views/SideBar/SideBar.tsx
+++ src/Views/SideBar/SideBar.tsx
@@ -1,28 +1,30 @@
-import Test from "../../Components/TestingComponent/Test";
+// import Test from "../../Components/TestingComponent/Test";
+import useSideBar from "../../Hooks/useSideBar";
 import { ROUTES } from "../../Shared/Constant";
-import Agency from "./componets/Agency";
-import Credit from "./componets/Credit";
-import Notification from "./componets/Notification";
+// import Agency from "./componets/Agency";
+// import Credit from "./componets/Credit";
+// import Notification from "./componets/Notification";
 import { PARAM_NAME, SideBarElement } from "./helper/constant"
 import { SideElement } from "./types/type"
 import {  useParams,useNavigate} from 'react-router-dom';
 function SideBar() {
-    const {tab}=useParams()
+    // const {tab}=useParams()
     const navigate=useNavigate()
-    const renderPage=()=>{
-        switch (tab) {
-            case PARAM_NAME.AGENCY:
-            return <Agency/>
-            break;
-            case PARAM_NAME.NOTIFCATION:
-            return <Notification/>
-            break;
-            case PARAM_NAME.Credit:
-            return <Credit/>
-            default:
-                return <Test/>
-        }
-    }
+    // const renderPage=()=>{
+    //     switch (tab) {
+    //         case PARAM_NAME.AGENCY:
+    //         return <Agency/>
+    //         break;
+    //         case PARAM_NAME.NOTIFCATION:
+    //         return <Notification/>
+    //         break;
+    //         case PARAM_NAME.Credit:
+    //         return <Credit/>
+    //         default:
+    //             return <Test/>
+    //     }
+    // }
+    const {renderPage,tab}=useSideBar()
   return (
     <div>
         {
